a5e28317ee3cd2ab1ed19692778d3ae11781dd18
akzize <akzakariae@gmail.com>
2024-01-15 11:59:08 +0100

    feat: altering the post table
    adding the user id in posts and the image

--- database/migrations/2024_01_15_104320_add_user_to_posts.php
+++ database/migrations/2024_01_15_104320_add_user_to_posts.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('content');
-
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
         });
     }
 
@@ -25,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
     }
 };
