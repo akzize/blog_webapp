c3fd30a103065bb74dd62166efdf6364b85f7a3b
akzize <akzakariae@gmail.com>
2024-01-15 11:50:52 +0100

    refactor(Post-and-User): adding the relations ship bwtween them

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,14 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'content'];
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
